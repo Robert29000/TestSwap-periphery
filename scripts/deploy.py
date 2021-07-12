6c704898da4e8f036965005116fe98a5fad0a7b2
Robert29000 <mayka29000@mail.ru>
2021-07-12 10:02:57 +0300

    fixed deploing scripts

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,6 +3,6 @@ from brownie import TestSwapRouter, accounts
 
 def main():
     test_account = accounts.add(os.getenv("PRIVATE_KEY"))
-    wbnb_address_test = '0xAaE79b63555C3e77Eb5011d78F21DaE16F5c4Ed1';
+    wbnb_address_test = '0xaF8980E881C6ef57B13A52b2c6541CDc49E91BDd';
     factory_address_test = '0x5359aD30170bCf38BE1A7E5C8CE8e2AC1ED2BeaE';
     token = TestSwapRouter.deploy(factory_address_test, wbnb_address_test, {'from': test_account})
